3704be6ea9bb2ec36f27dec2770ea69403799036
Satish Nepali <sat.nep001@gmail.com>
2024-05-09 16:25:54 +0545

    Update build_files.sh

--- build_files.sh
+++ build_files.sh
@@ -7,7 +7,7 @@ python3.9 -m venv venv
 source venv/bin/activate
 
 # install all deps in the venv
-pip install -r requirements.txt
+pip3 install -r requirements.txt
 
 # collect static files using the Python interpreter from venv
 python manage.py collectstatic --noinput
